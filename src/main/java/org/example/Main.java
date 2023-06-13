7436f60efe3ca89aaa4a92a647f05b09241eaf87
João Vertelo <joaovertelo@gmail.com>
2023-06-13 20:49:58 -0300

    commit 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Realizando autenticacao");
+        System.out.println("Listando metodos de pagamento");
     }
 }
